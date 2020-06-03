18ae8f4112cb35e47a85272846af1030bb9c1ee2
Nikola <nikolaviki@hotmail.com>
2020-06-03 11:37:10 +0200

    useReducer (simple state & action)

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import MouseContainer from './components/MouseContainer';
 import DataFetching from './components/DataFetching';
+import CounterOne from './components/CounterOne';
 // import HookCounter from './components/HookCounter';
 // import HookCounterTwo from './components/HookCounterTwo';
 // import HookCounterTheree from './components/HookCounterTheree';
 // import HookMouse from './components/HookMouse';
 // import HookEffect from './components/HookEffect';
+
 
 function App() {
   return (
@@ -17,7 +19,8 @@ function App() {
       {/* <HookEffect /> */}
       {/* <HookMouse></HookMouse> */}
       {/* <MouseContainer /> */}
-      <DataFetching />
+      {/* <DataFetching /> */}
+      <CounterOne />
     </div>
   );
 }
